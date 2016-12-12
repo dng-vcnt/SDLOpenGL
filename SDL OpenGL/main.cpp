57315f045714cef7ad0ebced9a142f82873436f7
Vincent Duong <vduong178@gmail.com>
2016-12-12 00:21:49 -0800

    Initial commit

--- SDL OpenGL/main.cpp
+++ SDL OpenGL/main.cpp
@@ -7,9 +7,62 @@
 //
 
 #include <iostream>
+#include <SDL2/sdl.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+#include <SDL2/SDL_opengl.h>
+
+const GLint WIDTH = 800, HEIGHT = 600;
+
+int main ()
+{
+    SDL_Init( SDL_INIT_EVERYTHING );
+    
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE );
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 3 );
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 3 );
+    SDL_GL_SetAttribute( SDL_GL_STENCIL_SIZE, 8 );
+    
+    SDL_Window *window = SDL_CreateWindow( "OpenGL", 0, 0, WIDTH, HEIGHT, SDL_WINDOW_OPENGL );
+    
+    SDL_GLContext context = SDL_GL_CreateContext( window );
+    
+    glewExperimental = GL_TRUE;
+    
+    if ( GLEW_OK != glewInit() )
+    {
+        std::cout << "Failed to initial GLEW" << std::endl;
+        return EXIT_FAILURE;
+    }
+    
+    glViewport( 0, 0, WIDTH, HEIGHT );
+    
+    SDL_Event windowEvent;
+    
+    while ( true )
+    {
+        if ( SDL_PollEvent( &windowEvent ) )
+        {
+            if ( SDL_QUIT == windowEvent.type )
+            {
+                break;
+            }
+        }
+        glClearColor( 0.2f, 0.3f, 0.2f, 1.0f );
+        glClear( GL_COLOR_BUFFER_BIT );
+        
+        // draw OpenGL
+        
+        SDL_GL_SwapWindow( window );
+    }
+    
+    SDL_GL_DeleteContext( context );
+    SDL_DestroyWindow( window );
+    SDL_Quit();
+    
+    return EXIT_SUCCESS;
 }
+
+
